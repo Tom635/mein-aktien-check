fb9b3ab569c58562605e10a00c978b89bd500bf1
Jan-Eric Eilers <149389152+Tom635@users.noreply.github.com>
2026-05-10 18:46:14 +0200

    Update Aktienbericht.py

--- Aktienbericht.py
+++ Aktienbericht.py
@@ -21,7 +21,7 @@ TICKERS = {
     "ENR.DE": "Siemens En", "SPOT": "Spotify", "TCEHY": "Tencent",
     "TSM": "TSMC", 'TM': "Toyota", "UCG.MI": "UniCredit", "UNP": "Union Pac",
     "V": "Visa", "WMT": "Walmart", "DIS": "Disney", "ZAL.DE": "Zalando",
-    "TAVHL.IS": "TAV Airp",
+    "TAVHL.IS": "TAV Airp", "VSCO": "Vicoria Secret", "KASPI": "Kasp",
 
     # Ergänzungen:
     "UBER": "Uber",
